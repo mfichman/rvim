63d08eb95a61068a89ef94d34240a4649681d73a
Matt Fichman <matt.fichman@gmail.com>
2014-01-28 14:41:59 -0500

    Fixed bug in cli.py

--- rvim/cli.py
+++ rvim/cli.py
@@ -1,13 +1,14 @@
 #!/bin/env python
 
 import rvim
+import sys
 
 def main():
     c = rvim.Client()
     for arg in sys.argv[1:]:
         try:
             c.open(os.path.abspath(arg))
-        except rvim.Exception, e:
+        except rvim.ClientException, e:
             sys.stderr.write('error: %s\n' % e.msg)
             sys.stderr.flush()
             sys.exit(1)
