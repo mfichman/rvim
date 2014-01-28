8d4c4be14d3b5b7374af8522ffd7842e316cdac2
Matt Fichman <matt.fichman@gmail.com>
2014-01-28 10:40:27 -0500

    Fixed a few problems with client/plugin

--- rvim/client.py
+++ rvim/client.py
@@ -9,6 +9,7 @@ import subprocess
 import watchdog.observers
 import watchdog.events
 import shutil
+import os
 
 class Client(watchdog.events.FileSystemEventHandler):
 
@@ -16,31 +17,50 @@ class Client(watchdog.events.FileSystemEventHandler):
         self.host = host
         self.port = port
         self.conn = httplib.HTTPConnection(host, port)
-        self.conn2 = httplib.HTTPConnection(host, port)
 
     def file(self, path):
         # Get a file by pathname
-        self.conn.request('GET', '/file/%s' % path)
-        return self.conn.getresponse().read()
+        try:
+            self.conn.request('GET', '/file/%s' % path)
+            return self.conn.getresponse().read()
+        except httplib.HTTPException, e:
+            self.conn.close()
+            raise
 
     def patch(self, path, patch):
         # Post a patch to the server.
-        data = json.dumps({'path': path, 'patch': patch})
-        headers = {'Content-Type': 'application/json'}
-        self.conn.request('POST', '/patch', data, headers)
-        return json.load(self.conn.getresponse())
+        try:
+            data = json.dumps({'path': path, 'patch': patch})
+            headers = {'Content-Type': 'application/json'}
+            self.conn.request('POST', '/patch', data, headers)
+            return json.load(self.conn.getresponse())
+        except httplib.HTTPException, e:
+            self.conn.close()
+            raise
 
     def next(self, id):
         # Wait for the next file to open
-        self.conn2.request('GET', '/open/%d' % id)
-        return json.load(self.conn2.getresponse())
+        try:
+            self.conn.request('GET', '/open/%d' % id)
+            return json.load(self.conn2.getresponse())
+        except httplib.HTTPException, e:
+            self.conn.close()
+            raise
 
     def run(self):
         # Wait for commands from the remote server.
         id = 0
         while True:
             data = self.next(id)
+            os.environ['RVIM'] = '1'
+            os.environ['RVIMHOST'] = self.host
+            os.environ['RVIMPORT'] = str(self.port)
             print data['path']
             subprocess.Popen(('gvim', data['path']), shell=True)
 
+def main():
+    c = Client('obs20', 40000)
+    c.run()
 
+if __name__ == '__main__':
+    main()
--- rvim/vimplugin.py
+++ rvim/vimplugin.py
@@ -6,6 +6,9 @@ import re
 import threading
 import difflib
 
+
+data_path = os.path.join(os.path.expanduser('~'), '.rvimdata')
+
 def main():
     # Entrypoint for the vim plugin
     global client
@@ -14,15 +17,10 @@ def main():
     client = rvim.Client(host, port)
     vim.command('au BufReadCmd * :py rvim.vimplugin.buf_read_cmd()')
     vim.command('au BufWriteCmd * :py rvim.vimplugin.buf_write_cmd()')
-
-    th = threading.Thread(target=client.run)
-    th.daemon = True
-    th.start()
     buf_read_cmd()
 
 def write(path, data):
     # Write a file to the disk, and create any directories necessary.
-    path = os.path.join(os.path.expanduser('~'), '.data', path)
     (dirname, _) = os.path.split(path)
     try:
         os.makedirs(dirname)
@@ -33,39 +31,46 @@ def write(path, data):
 
 def read(path):
     # Read a file from disk; throw an IOError if the file cannot be read.
-    path = os.path.join('.data', path)
     with open(path) as fd:
         return fd.read()
 
 def remote_file_path(path):
     # Convert a local file path to a remote one.  
-    cwd = os.getcwd()
-    path = path.replace('%s\\' % cwd, '')
-    path = re.sub(r'\\', '/', path)
-    path = re.sub(r"^[A-Z]:/", '', path)
-    return path 
+    path = path.replace(data_path, '')
+    path = path.replace('\\', '/')
+    return path
+
+def local_file_path(path):
+    path = path.replace(data_path, '')
+    path = re.sub(r"^[A-Z]:\\", '', path)
+    path = os.path.join(data_path, path)
+    return path
 
 def buf_read_cmd():
     # Read the file from the server
-    path = remote_file_path(vim.current.buffer.name)
-    data = client.file(path)
-    vim.current.buffer[:] = data.split('\n')
-    write('%s.base' % path, data)
-    write(path, data)
-    vim.command('set nomodified')
+    if vim.current.buffer.name:
+        path = local_file_path(vim.current.buffer.name)
+        data = client.file(remote_file_path(path))
+        try:
+            os.remove(path)
+        except OSError, e:
+            pass
+        write('%s.base' % path, data)
+        vim.current.buffer[:] = data.split('\n')
+        vim.current.buffer.name = path
+        vim.command('set nomodified')
     
 def buf_write_cmd():
     # Save diff of the current file to the server
-    path = remote_file_path(vim.current.buffer.name)
+    path = vim.current.buffer.name
     data = '\n'.join(vim.current.buffer)+'\n'
     write(path, data)
     new = data.splitlines(True)
     base = read('%s.base' % path).splitlines(True)
-
     patch = ''.join(difflib.unified_diff(base, new))
     if patch:
-        client.patch(path, patch) 
-    write('%s.base' % path, data)
+        client.patch(remote_file_path(path), patch) 
+        write('%s.base' % path, data)
     vim.command('set nomodified')
 
 if 'RVIM' in os.environ:
